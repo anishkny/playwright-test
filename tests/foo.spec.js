9ed7617c79d1d4f9a6213294e9c048bd0a97f780
Anish Karandikar <anishkny@gmail.com>
2022-05-26 07:42:52 -0700

    fix: Remove screenshot test

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -4,5 +4,4 @@ test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const name = await page.innerText('.navbar__title');
   expect(name).toBe('Playwright');
-  expect(await page.screenshot()).toMatchSnapshot('snapshot-name.png');
 });
